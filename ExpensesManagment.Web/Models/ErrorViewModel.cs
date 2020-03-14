3f60087925f51646ba2d1be2b985c450401587dc
Alexander GL <yagarcia1402@gmail.com>
2020-03-13 20:18:50 -0500

    Adding  Project Architecture

---
+++ ExpensesManagment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExpensesManagment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
